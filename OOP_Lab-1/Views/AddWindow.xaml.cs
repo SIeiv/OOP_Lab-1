e343098f09d44ceeb99d42af4d22c7235b11cf84
Kanno <takidawakanno@gmail.com>
2025-02-17 11:06:25 +0300

    Weakness improvement

--- OOP_Lab-1/Views/AddWindow.xaml.cs
+++ OOP_Lab-1/Views/AddWindow.xaml.cs
@@ -1,57 +1,72 @@
-﻿using System.Runtime.InteropServices.JavaScript;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
+﻿using System.Runtime.InteropServices;
+using System.Windows.Input;
 using OOP_Lab_1.Models;
 using OOP_Lab_1.Models.Components;
 using OOP_Lab_1.Models.Enums;
+using OOP_Lab_1.ViewModels;
+using static System.Enum;
 
 namespace OOP_Lab_1.Views;
 
-public partial class AddWindow : Window
+public partial class AddWindow
 {
-    public MainWindow CustomOwner;
-    public AddWindow()
+    public ICommand AddCommand => new RelayCommand(_ => AddComputer() );
+
+    
+    
+    private readonly MainWindow _customOwner;
+    public AddWindow(MainWindow owner)
     {
+        _customOwner = owner;
+        DataContext = this;
         InitializeComponent();
     }
 
-    private void AddButton_OnClick(object sender, RoutedEventArgs e)
+    private void AddComputer()
     {
         try
         {
-            Manufacturer ProcessorManufact;
-            Manufacturer.TryParse(ProcessorManufactComboBox.Text, out ProcessorManufact);
+            TryParse(ProcessorManufactComboBox.Text, out Manufacturer processorManufacturer);
 
-            MemoryType memoryType;
-            MemoryType.TryParse(RamTypeComboBox.Text, out memoryType);
+            TryParse(RamTypeComboBox.Text, out MemoryType memoryType);
+
+            TryParse(StorageTypeComboBox.Text, out StorageType storageType);
+
+            TryParse(CompManufactComboBox.Text, out Manufacturer compManufacturer);
             
-            StorageType storageType;
-            StorageType.TryParse(StorageTypeComboBox.Text, out storageType);
-            
-            Manufacturer CompManufact;
-            Manufacturer.TryParse(CompManufactComboBox.Text, out CompManufact);
-            
-            Processor p1 = new Processor(ProcessorManufact, ProcessorModelTextBox.Text,
-                Int16.Parse(ProcessorFrequencyTextBox.Text));
-            Ram r1 = new Ram(Int16.Parse(RamSizeTextBox.Text), memoryType);
+            var p1 = new Processor(processorManufacturer, ProcessorModelTextBox.Text,
+                int.Parse(ProcessorFrequencyTextBox.Text));
+            var r1 = new Ram(int.Parse(RamSizeTextBox.Text), memoryType);
         
-            Computer c1 = new Computer();
-            c1.CPU = p1;
-            c1.Memory = r1;
-        
-            c1.StorageGB = Int16.Parse(StorageSizeTextBox.Text);
-            c1.StorageType = storageType;
-            c1.Manufacturer = CompManufact;
-            c1.Price = Int32.Parse(ComputerPriceTextBox.Text);
-            
-            CustomOwner.Computers.Add(c1);
-            CustomOwner.MainList.Items.Refresh();;
+            var c1 = new Computer
+            {
+                CPU = p1,
+                Memory = r1,
+                StorageGB = short.Parse(StorageSizeTextBox.Text),
+                StorageType = storageType,
+                Manufacturer = compManufacturer,
+                Price = short.Parse(ComputerPriceTextBox.Text)
+            };
+
+            _customOwner.Computers.Add(c1);
+            _customOwner.MainList.Items.Refresh();
             Close();
         }
         catch (Exception exception)
         {
-            MessageBox.Show(exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            ShowNativeMessageBox("Большой секс", exception.Message);
         }
     }
+    
+    
+    
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+    private static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+    private void ShowNativeMessageBox(string title, string message)
+    {
+        MessageBox(IntPtr.Zero, message, title, 0x00000040);
+    }
+    
+    
 }
